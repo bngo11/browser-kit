664dd293bdae27152794ecc33e177aac07b38c74
shadow <shadowngoz@gmail.com>
2022-06-26 21:59:16 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9ea736991281cf349dc1a7c64f164c62c119407d
      kit-fixups: eaade9dc9e98cabd5f6eb92f36ab255aa00a65ab

--- www-client/w3m/generator.py
+++ www-client/w3m/generator.py
@@ -8,14 +8,12 @@ async def generate(hub, **pkginfo):
     github = f"https://api.github.com/repos/{github_user}/{github_repo}"
 
     pkgmetadata = await hub.pkgtools.fetch.get_page(github, is_json=True)
-    vers, data = await hub.pkgtools.github.latest_tag_version(hub, github_user, github_repo)
-
     newpkginfo = await hub.pkgtools.github.tag_gen(hub, github_user, github_repo)
 
     ebuild = hub.pkgtools.ebuild.BreezyBuild(
         **pkginfo,
         description=pkgmetadata["description"],
-        version=data.get("name")[1:].replace('+git', '_p'),
+        version=newpkginfo['version'],
         artifacts=[newpkginfo['artifacts'][0]]
     )
 
